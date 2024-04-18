241d58d568e468766b1125ba2dc89b72aac6038a
dacortes <dannyel.cortesr@gmail.com>
2024-04-18 12:08:22 +0200

    [ADD] ex00

---
+++ ex00/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 10:02:26 by dacortes          #+#    #+#             */
+/*   Updated: 2024/04/18 11:57:09 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../inc/whatever.hpp"
+
+int main (void)
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return (EXIT_SUCCESS);
+}
+
